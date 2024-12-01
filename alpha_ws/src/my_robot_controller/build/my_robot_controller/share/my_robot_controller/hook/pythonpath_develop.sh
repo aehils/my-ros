adffde0d4a68626584f0cd25d555a4943ec622df
Eli <okara.elisha@gmail.com>
2024-12-01 13:12:15 +0000

    workspace source folder

---
+++ alpha_ws/src/my_robot_controller/build/my_robot_controller/share/my_robot_controller/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/elishaokara.linux/alpha_ws/src/my_robot_controller/build/my_robot_controller"
